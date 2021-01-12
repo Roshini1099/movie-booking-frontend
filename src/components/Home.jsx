753a077dfec9887a7fe50af56921048609809b93
Roshini1099 <65917489+Roshini1099@users.noreply.github.com>
2021-01-13 02:13:26 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import {Container}from 'react-bootstrap';
-import logo from './logo.jpg';
 export default class Home extends Component{
     render()
     {
         return(
-            <div>
-                    <Container>
-                        <hr></hr>
-                        <h1>Welcome to Cinematics!!!!!</h1>
-                        <p>
-
-                            Bringing the World Inside!!!
-                     
-                        </p>
-                    </Container>
-                <img src={logo} alt="Logo" />;
+            <div  style={{ backgroundImage: "url(/bg.jpg)",height: '100%', position: 'absolute', left: '0px', width: '100%', overflow: 'hidden'}}>
+                    
             </div>
         )
     }
